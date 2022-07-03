13da688eebca6b7ba53d45653107abf77c785496
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-04 00:28:27 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,5 +7,5 @@ from telethon import TelegramClient
 from pyrogram import Client
 from config import Config
 
-bot = TelegramClient("AutoAnime", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN")
+bot = TelegramClient("AutoAnime", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN"))
 app = Client("Auto-Anime", bot_token=Config.get("BOT_TOKEN"), api_id=Config.get("API_ID"), api_hash=Config.get("API_HASH"))
