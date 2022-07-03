ddcb2e2feb672f6d47df99697a1e961653de2ce1
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 23:42:55 +0530

    Create __init__.py

---
+++ main/plugins/__init__.py
@@ -0,0 +1,121 @@
+from main import *
+
+import inspect, re, asyncio, time, sys, os
+from pathlib import Path
+from telethon import events
+from asyncio import sleep
+from ..fast_telethon import uploader, downloader
+
+from telethon.errors import MessageDeleteForbiddenError, MessageNotModifiedError
+from telethon.tl.custom import Message
+from telethon.tl.types import MessageService
+
+async def bash(cmd, run_code=0):
+    """
+    run any command in subprocess and get output or error."""
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip() or None
+    out = stdout.decode().strip()
+    if not run_code and err:
+        split = cmd.split()[0]
+        if f"{split}: not found" in err:
+            return out, f"{split.upper()}_NOT_FOUND"
+    return out, err
+
+def admin_cmd(pattern=None, command=None, **args):
+    args["func"] = lambda e: e.via_bot_id is None
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith(r"\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        elif pattern.startswith(r"^"):
+            args["pattern"] = re.compile(pattern)
+            cmd = pattern.replace("$", "").replace("^", "").replace("\\", "")
+        else:
+            if len(Config.get("HANDLER")) == 2:
+                darkreg = "^" + Config.get("HANDLER")
+                reg = Config.get("HANDLER").split(" ")[1]
+            elif len(Config.get("HANDLER")) == 1:
+                darkreg = "^\\" + Config.get("HANDLER")
+                reg = Config.get("HANDLER")
+            args["pattern"] = re.compile(darkreg + pattern)
+            if command is not None:
+                cmd = reg + command
+            else:
+                cmd = (
+                    (reg + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                )
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = SUDOS
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    args["blacklist_chats"] = True
+    black_list_chats = list(Config.get("BLACKLIST_CHATS") or "")
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+
+    # add blacklist chats, UB should not respond in these chats
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+
+    return events.NewMessage(**args)
+
+async def eor(event, text=None, **args):
+    time = args.get("time", None)
+    edit_time = args.get("edit_time", None)
+    if "edit_time" in args:
+        del args["edit_time"]
+    if "time" in args:
+        del args["time"]
+    if "link_preview" not in args:
+        args["link_preview"] = False
+    args["reply_to"] = event.reply_to_msg_id or event
+    if event.out and not isinstance(event, MessageService):
+        if edit_time:
+            await sleep(edit_time)
+        if "file" in args and args["file"] and not event.media:
+            await event.delete()
+            ok = await event.client.send_message(event.chat_id, text, **args)
+        else:
+            try:
+                try:
+                    del args["reply_to"]
+                except KeyError:
+                    pass
+                ok = await event.edit(text, **args)
+            except MessageNotModifiedError:
+                ok = event
+    else:
+        ok = await event.client.send_message(event.chat_id, text, **args)
+
+    if time:
+        await sleep(time)
+        return await ok.delete()
+    return ok
+
+async def eod(event, text=None, **kwargs):
+    kwargs["time"] = kwargs.get("time", 8)
+    return await eor(event, text, **kwargs)
