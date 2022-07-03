cb778de0b5d7148f731e108c50bacca348392138
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 23:31:13 +0530

    Create __init__.py

---
+++ main/__init__.py
@@ -0,0 +1,10 @@
+import os
+import time
+import sys
+import shutil
+
+from telethon import TelegramClient
+from pyrogram import Client
+
+bot = TelegramClient("AutoAnime", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN")
+app = Client("Auto-Anime", bot_token=Config.get("BOT_TOKEN"), api_id=Config.get("API_ID"), api_hash=Config.get("API_HASH"))
