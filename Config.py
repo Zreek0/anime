8558bfcd17477e5d1fffab5e8768d11e2b20092e
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 23:08:17 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,10 @@
+from os import environ
+from dotenv import load_dotenv, find_dotenv, dotenv_values
+
+ENV = environ.get("ENV")
+if not ENV:
+	load_dotenv(find_dotenv())
+else:
+	pass
+Config = environ
+
