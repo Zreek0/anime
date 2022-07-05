b7c48b68d4f3ccfd9b87bdd307b9245052021ffa
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-05 12:46:54 +0530

    Update __init__.py

--- main/plugins/__init__.py
+++ main/plugins/__init__.py
@@ -179,7 +179,7 @@ async def upload_gogoanime(entry, gogolink, notif_chat, upload_chat):
 		name = "[@Ongoing_Seasonal_Anime] " + entry.title + " (1080p).mp4"
 		path = os.path.join(os.getcwd(), name)
 		obj = SmartDL(dllink, path, progress_bar=False)
-		obi.start()
+		obj.start()
 		thumb = generate_thumbnail(path, name + ".jpg")
 		caption = f"**{entry.title}**\n\n**• Qᴜᴀʟɪᴛʏ :** 1080p\n**• ᴀᴜᴅɪᴏ :** Japanese\n**• ꜱᴜʙᴛɪᴛʟᴇꜱ :** English"
 		xx = await uploader(name, name, time.time(), mess, "Uploading... " + name)
@@ -191,7 +191,7 @@ async def upload_gogoanime(entry, gogolink, notif_chat, upload_chat):
 		name = "[@Ongoing_Seasonal_Anime] " + entry.title + " (720p).mp4"
 		path = os.path.join(os.getcwd(), name)
 		obj = SmartDL(dllink, path, progress_bar=False)
-		obi.start()
+		obj.start()
 		thumb = generate_thumbnail(path, name + ".jpg")
 		caption = f"**{entry.title}**\n\n**• Qᴜᴀʟɪᴛʏ :** 720p\n**• ᴀᴜᴅɪᴏ :** Japanese\n**• ꜱᴜʙᴛɪᴛʟᴇꜱ :** English"
 		xx = await uploader(name, name, time.time(), mess, "Uploading... " + name)
@@ -203,7 +203,7 @@ async def upload_gogoanime(entry, gogolink, notif_chat, upload_chat):
 		name = "[@Ongoing_Seasonal_Anime] " + entry.title + " (480p).mp4"
 		path = os.path.join(os.getcwd(), name)
 		obj = SmartDL(dllink, path, progress_bar=False)
-		obi.start()
+		obj.start()
 		thumb = generate_thumbnail(path, name + ".jpg")
 		caption = f"**{entry.title}**\n\n**• Qᴜᴀʟɪᴛʏ :** 480p\n**• ᴀᴜᴅɪᴏ :** Japanese\n**• ꜱᴜʙᴛɪᴛʟᴇꜱ :** English"
 		xx = await uploader(name, name, time.time(), mess, "Uploading... " + name)
