5e50da0ac2d4e675bd859c679f9427dd37f6228a
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-21 13:23:30 +0530

    Update helper.py

--- main/plugins/helper.py
+++ main/plugins/helper.py
@@ -65,8 +65,7 @@ async def post_ws(link, name, chapter, class_="wp-manga-chapter-img", src="src")
 		i = i[src].split("\t")[-1]
 		n += 1
 		file = open(f"./{upr}/{n}.jpg", "wb")
-		executor = ThreadPoolExecutor(max_workers=40)
-		executor.submit(download, i, file.name, dict(Referer=r.url))
+		await fast_download(i, file.name, headers=dict(Referer=r.url))
 		images.append(file.name)
 	with open(pdfname, "wb") as f:
 		try:
